1b8f79369de845fac30ee004a6645f7925a16a5a
Geoffrey Floyd <geoffrey.floyd@hoomanlogic.com>
2016-07-11 13:04:27 -0600

    domain swtich

--- server.config.js
+++ server.config.js
@@ -1,6 +1,6 @@
 module.exports = exports = function (debug) {
    return {
-      hostName: debug ? 'localhost' : 'home.hoomanlogic.com',
+      hostName: debug ? 'localhost' : 'home.gnidbits.com',
       port: debug ? 8081 : 80,
       socketPort: debug ? 8080 : 8181,
    };
