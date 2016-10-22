17824cc7289b17dd45ee270b185bc723311e8810
Geoffrey Floyd <geoffrey.floyd@hoomanlogic.com>
2016-10-22 11:15:43 -0600

    change site to hooman.io

--- server.config.js
+++ server.config.js
@@ -1,6 +1,6 @@
 module.exports = exports = function (debug) {
    return {
-      hostName: debug ? 'localhost' : 'home.gnidbits.com',
+      hostName: debug ? 'localhost' : 'home.hooman.io',
       port: debug ? 8081 : 80,
       socketPort: debug ? 8080 : 8181,
    };
